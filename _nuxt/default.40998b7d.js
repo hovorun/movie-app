9f3b900233b88aa32850d8b9d25ad340b1f8dc6e
v_hovorun <volodymyr@nayaone.com>
2023-03-19 20:18:37 +0200

    Updated static content

---
+++ _nuxt/default.40998b7d.js
@@ -0,0 +1,1 @@
+import{a as s,b as t,K as o,o as a}from"./entry.a1d95ffb.js";const c={},r={class:"app"};function n(e,_){return a(),t("div",r,[o(e.$slots,"default")])}const d=s(c,[["render",n]]);export{d as default};
